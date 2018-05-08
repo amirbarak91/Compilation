3d7049554d6ff31d144511c4d23b9041a813be56
Amir <Amirbarak91@gmail.com>
2018-05-08 16:57:52 +0300

    Change HashTable Methods Names

--- HashTable.c
+++ HashTable.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include "HashTable.h"
 
-static unsigned int ht_calc_hash(char* key)
+static unsigned int HashTable_Calc_Hash(char* key)
 {
 	unsigned int h = 5381;
 	while (*(key++))
@@ -12,8 +12,7 @@ static unsigned int ht_calc_hash(char* key)
 
 /* 	Create a hashtable with capacity 'capacity'
 and return a pointer to it*/
-
-hashtable_t* ht_create(unsigned int capacity)
+hashtable_t* Create_HashTable(unsigned int capacity)
 {
 	hashtable_t* hasht = malloc(sizeof(hashtable_t));
 	if (!hasht)
@@ -34,7 +33,7 @@ hashtable_t* ht_create(unsigned int capacity)
 /* 	Store data in the hashtable. If data with the same key are already stored,
 they are overwritten, and return by the function. Else it return NULL.
 Return HT_ERROR if there are memory alloc error*/
-void* ht_put(hashtable_t* hasht, char* key, void* data)
+void* HashTable_Put(hashtable_t* hasht, char* key, void* data)
 {
 	if (data == NULL)
 		return NULL;
@@ -68,7 +67,7 @@ void* ht_put(hashtable_t* hasht, char* key, void* data)
 }
 
 /* Retrieve data from the hashtable */
-void* ht_get(hashtable_t* hasht, char* key)
+void* HashTable_Get(hashtable_t* hasht, char* key)
 {
 	unsigned int h = ht_calc_hash(key) % hasht->capacity;
 	hash_elem_t* e = hasht->table[h];
@@ -83,9 +82,9 @@ void* ht_get(hashtable_t* hasht, char* key)
 
 /* 	Remove data from the hashtable. Return the data removed from the table
 so that we can free memory if needed */
-void* ht_remove(hashtable_t* hasht, char* key)
+void* HashTable_Remove(hashtable_t* hasht, char* key)
 {
-	unsigned int h = ht_calc_hash(key) % hasht->capacity;
+	unsigned int h = HashTable_Calc_Hash(key) % hasht->capacity;
 	hash_elem_t* e = hasht->table[h];
 	hash_elem_t* prev = NULL;
 	while (e != NULL)
@@ -109,7 +108,7 @@ void* ht_remove(hashtable_t* hasht, char* key)
 }
 
 /* List keys. k should have length equals or greater than the number of keys */
-void ht_list_keys(hashtable_t* hasht, char** k, size_t len)
+void HashTable_List_Keys(hashtable_t* hasht, char** k, size_t len)
 {
 	if (len < hasht->e_num)
 		return;
@@ -128,7 +127,7 @@ void ht_list_keys(hashtable_t* hasht, char** k, size_t len)
 
 /* 	List values. v should have length equals or greater
 than the number of stored elements */
-void ht_list_values(hashtable_t* hasht, void** v, size_t len)
+void HashTable_List_Values(hashtable_t* hasht, void** v, size_t len)
 {
 	if (len < hasht->e_num)
 		return;
@@ -146,7 +145,7 @@ void ht_list_values(hashtable_t* hasht, void** v, size_t len)
 }
 
 /* Iterate through table's elements. */
-hash_elem_t* ht_iterate(hash_elem_it* iterator)
+hash_elem_t* HashTable_Iterate(hash_elem_it* iterator)
 {
 	while (iterator->elem == NULL)
 	{
@@ -165,37 +164,41 @@ hash_elem_t* ht_iterate(hash_elem_it* iterator)
 }
 
 /* Iterate through keys. */
-char* ht_iterate_keys(hash_elem_it* iterator)
+char* HashTable_Interate_Keys(hash_elem_it* iterator)
 {
-	hash_elem_t* e = ht_iterate(iterator);
+	hash_elem_t* e = HashTable_Iterate(iterator);
 	return (e == NULL ? NULL : e->key);
 }
 
 /* Iterate through values. */
-void* ht_iterate_values(hash_elem_it* iterator)
+void* HashTable_Interate_Values(hash_elem_it* iterator)
 {
-	hash_elem_t* e = ht_iterate(iterator);
+	hash_elem_t* e = HashTable_Iterate(iterator);
 	return (e == NULL ? NULL : e->data);
 }
 
 /* 	Removes all elements stored in the hashtable.
 if free_data, all stored datas are also freed.*/
-void ht_clear(hashtable_t* hasht, int free_data)
+void HashTable_Clear(hashtable_t* hasht, int free_data)
 {
 	hash_elem_it it = HT_ITERATOR(hasht);
-	char* k = ht_iterate_keys(&it);
+	char* k = HashTable_Iterate(&it);
 	while (k != NULL)
 	{
-		free_data ? free(ht_remove(hasht, k)) : ht_remove(hasht, k);
-		k = ht_iterate_keys(&it);
+		if(free_data)
+			free(HashTable_Remove(hasht, k));
+		else
+			HashTable_Remove(hasht, k);
+
+		k = HashTable_Interate_Keys(&it);
 	}
 }
 
 /* 	Destroy the hash table, and free memory.
 Data still stored are freed*/
-void ht_destroy(hashtable_t* hasht)
+void HashTable_Destroy(hashtable_t* hasht)
 {
-	ht_clear(hasht, 1); // Delete and free all.
+	Hashtable_Clear(hasht, 1); // Delete and free all.
 	free(hasht->table);
 	free(hasht);
 }
--- HashTable.h
+++ HashTable.h
@@ -29,19 +29,18 @@ char err_ptr;
 void* HT_ERROR = &err_ptr; // Data pointing to HT_ERROR are returned in case of error
 
 
-static unsigned int ht_calc_hash(char* key);
-
-hashtable_t* ht_create(unsigned int capacity);
-void* ht_put(hashtable_t* hasht, char* key, void* data);
-void* ht_get(hashtable_t* hasht, char* key);
-void* ht_remove(hashtable_t* hasht, char* key);
-void ht_list_keys(hashtable_t* hasht, char** k, size_t len);
-void ht_list_values(hashtable_t* hasht, void** v, size_t len);
-hash_elem_t* ht_iterate(hash_elem_it* iterator);
-char* ht_iterate_keys(hash_elem_it* iterator);
-void* ht_iterate_values(hash_elem_it* iterator);
-void ht_clear(hashtable_t* hasht, int free_data);
-void ht_destroy(hashtable_t* hasht);
+static unsigned int HashTable_Calc_Hash(char* key);
+hashtable_t* Create_HashTable(unsigned int capacity);
+void* HashTable_Put(hashtable_t* hasht, char* key, void* data);
+void* HashTable_Get(hashtable_t* hasht, char* key);
+void* HashTable_Remove(hashtable_t* hasht, char* key);
+void HashTable_List_Keys(hashtable_t* hasht, char** k, size_t len);
+void HashTable_List_Values(hashtable_t* hasht, void** v, size_t len);
+hash_elem_t* HashTable_Iterate(hash_elem_it* iterator);
+char* HashTable_Interate_Keys(hash_elem_it* iterator);
+void* HashTable_Interate_Values(hash_elem_it* iterator);
+void HashTable_Clear(hashtable_t* hasht, int free_data);
+void HashTable_Destroy(hashtable_t* hasht);
 
 
 
